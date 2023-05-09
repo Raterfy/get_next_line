0d5a48f49b34dbadd548bdf411b4ca3462b86c31
Raterfy <roberthak02@gmail.com>
2023-05-09 23:41:36 +0200

    changement de nom de variable comme de nom de partie bytes_read

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:32:48 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/09 23:22:35 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/09 23:41:01 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 char	*get_next_line(int fd)
 {
 	static char	save;
-	char buffer[BUFFER_SIZE + 1];
-	int			n;
+	char		buffer[BUFFER_SIZE + 1];
+	int			bytes_read;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return(NULL);
-	n = read(fd, buffer, BUFFER_SIZE);
-	if (n < 0)
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
 		return (NULL);
-	buffer[n] = '\0';
+	buffer[bytes_read] = '\0';
 	
 }
