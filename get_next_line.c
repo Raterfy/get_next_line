e75830115489ae06b3b2198b0bdb25ca7491516a
Raterfy <roberthak02@gmail.com>
2023-05-09 23:30:55 +0200

    buffer[BUFFER_SIZE]

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,22 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:32:48 by robhak            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/05/07 20:53:47 by robhak           ###   ########.fr       */
-=======
-/*   Updated: 2023/05/09 12:41:22 by robhak           ###   ########.fr       */
->>>>>>> a615563c53338fb6c87467c140bdcbad23e96cca
+/*   Updated: 2023/05/09 23:22:35 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-<<<<<<< HEAD
+	static char	save;
+	char buffer[BUFFER_SIZE + 1];
+	int			n;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return(NULL);
-	
-}
-=======
-	char *buff;
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	n = read(fd, buffer, BUFFER_SIZE);
+	if (n < 0)
 		return (NULL);
-}
-
-int main(void)
-{
-	int	fd;
-	fd = open("test.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		write(1,"Error open", 10);
-		return (0);
-	}
-	char	buffer[100];
-	int n;
+	buffer[n] = '\0';
 	
 }
->>>>>>> a615563c53338fb6c87467c140bdcbad23e96cca
