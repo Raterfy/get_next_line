6e15e902b31d41a816d0a2ea401e486aaa85d0a9
Robert Hak <robhak@e3r9p10.clusters.42paris.fr>
2023-05-10 14:30:32 +0200

    changement boucle while l39

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:32:48 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/10 14:07:12 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/10 14:30:02 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*get_next_line(int fd)
 	if (bytes_read < 0)
 		return (NULL);
 	buffer[bytes_read] = '\0';
-	while (bytes_read > 0)
+	while (1)
 	{
 		if (save)//cas ou la variable statique save n'est pas NULL;
 		{
