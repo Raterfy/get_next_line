647fd2e956642adb9eee2da9f4d87ef79b389a76
Raterfy <roberthak02@gmail.com>
2023-05-10 23:15:39 +0200

    fonction utile

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,21 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 08:07:02 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/10 11:12:43 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/10 22:50:45 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
 
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include<unistd.h>
-# include<stdlib.h>
-# include<stdio.h>
-# include<string.h>
-# include <stdarg.h>
-# include <fcntl.h>
+# include <stdio.h>
+# include <stddef.h>
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1048
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,43 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 08:07:19 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/10 14:57:18 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/10 22:58:52 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	gnl_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*gnl_strjoin(char *s1, char *s2)
+{
+	char	*s;
+	int		i;
+	int		j;
+
+	s = malloc(gnl_stren(s1) + gnl_strlen(s2) + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		s[i] = s1[i];
+	}
+	free (s1);
+	j = 0;
+	while (s2[j])
+	{
+		s[i + j] = s2[j];
+		j++;
+	}
+	s[i + j] = '\0';
+	return (s);
+}
