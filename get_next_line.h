bb2e47ca1262c291750756bf22d245cf83ce804c
Robert Hak <robhak@e1r8p11.clusters.42paris.fr>
2023-05-11 10:05:23 +0200

    endif dans la fonction pour # ifndef GET_NEXT_LINE_H

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 08:07:02 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/11 00:39:00 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/11 10:04:11 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+# ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <sys/types.h>
@@ -30,4 +30,5 @@ char	*gnl_strncpy(char *dest, const char *src, size_t n);
 char	*gnl_strchr(const char *s, int c);
 char	*gnl_strjoin(char *s1, char *s2);
 
+#endif
 #endif
