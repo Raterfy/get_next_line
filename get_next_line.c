9b48d053124e31e805bb05b54986e0d7b8fd25f5
Raterfy <roberthak02@gmail.com>
2023-05-14 14:47:18 +0200

    je reset tout

--- get_next_line.c
+++ get_next_line.c
@@ -6,179 +6,29 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:32:48 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/12 16:30:07 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/14 14:46:36 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*static char	*gnl_readline(char *residual_str, int fd)
+
+
+int	main()
 {
-	char	buffer[BUFFER_SIZE + 1];
-	char	*newline;
-	char	*tmp;
-	int		byte_read;
-
-	byte_read = read(fd, buffer, BUFFER_SIZE);
-	newline = gnl_strchr(residual_str, '\n');
-	while (byte_read > 0)
-	{
-		buffer[byte_read] = '\0';
-		tmp = gnl_strjoin(residual_str, buffer);
-		free(residual_str);
-		residual_str = tmp;
-		if (newline)
-			break ;
-	}
-	if (byte_read <= 0 && !newline)
-		return (NULL);
-	if (newline)
-		*newline = '\0';
-	return (residual_str);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*residual_string;
-	char		*line;
-	char		*newline;
-
-	newline = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	if (!residual_string || !(newline = gnl_strchr(residual_string, '\n')))
-		residual_string = gnl_readline(residual_string, fd);
-	if (!residual_string)
-		return (NULL);
-	if (newline)
-	{
-		line = gnl_strncpy(gnl_strdup(residual_string),
-				residual_string, newline - residual_string + 1);
-		residual_string = gnl_strcpy(residual_string, newline + 1);
-	}
-	else
-	{
-		line = gnl_strdup(residual_string);
-		free(residual_string);
-		residual_string = NULL;
-	}
-	return (line);
-}*/
-/*
-static int	read_buffer(int fd, char *buffer, char **residual_string)
-{
-	int	bytes_read;
-
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read < 0)
-		return (-1);
-	buffer[bytes_read] = '\0';
-	if (!residual_string)
-		*residual_string = gnl_strdup("");
-	*residual_string = gnl_strjoin(*residual_string, buffer);
-	//free(buffer);
-	return (bytes_read);
-}
-
-static char	*extract_line(char **residual_string)
-{
-	char	*new_line;
-	char	*tmp;
-
-	//extract line doesn't extract the line
-	new_line = gnl_strchr(*residual_string, '\n');
-	//printf("new line = %s\n", new_line);
-	if (new_line)
-	{
-		tmp = gnl_strncpy(*residual_string, new_line + 1, gnl_strlen(new_line));
-		if (!tmp)
-			return (NULL);
-	}
-	else
-	{
-		tmp = gnl_strdup(*residual_string);
-		if (!tmp)
-			return (NULL);
-	}
-	return (tmp);
-}
-
-static char	*get_next_line_loop(int fd, char **residual_string)
-{
-	char	*new_line;
-	char	*tmp;
-	char	*buffer;
-	int		bytes_read;
-
-	bytes_read = 1;
-	while (bytes_read > 0)
-	{
-		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buffer)
-			return (NULL);
-		bytes_read = read_buffer(fd, buffer, residual_string);
-		if (bytes_read == -1)
-			return (NULL);
-		if (gnl_strchr(*residual_string, '\n'))
-			break ;
-	}
-	if (!bytes_read && !gnl_strlen(*residual_string))
-		return (NULL);
-	new_line = extract_line(residual_string);
-	tmp = *residual_string;
-	*residual_string = NULL;
-//	free(tmp);
-	return (new_line);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*residual_string;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	return (get_next_line_loop(fd, &residual_string));
-}*/
-
-/*static char	*append_line()
-{
-	
-}
-
-static char	*readline(int fd, char **residual_string)
-{
-	
-}	
-
-char	*get_next_line(fd)
-{
-	static char	*residual_string;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	
-}*/
-
-int	main(int argc, char **argv)
-{
-	int		fd;
+	int		fd = open("test.txt", O_RDONLY);
 	char	*line;
 
-	if (argc != 2)
+	while (1)
 	{
-		printf("Usage: %s <filename>\n", argv[0]);
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Failed to open file: %s\n", argv[1]);
-		return (1);
-	}
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		printf("line : %s\n", line);
-		free(line);
+		line = get_next_line(fd);
+		if (line)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
+		else
+			break;
 	}
 	close(fd);
 	return (0);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: robhak <robhak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 08:07:02 by robhak            #+#    #+#             */
-/*   Updated: 2023/05/12 16:31:08 by robhak           ###   ########.fr       */
+/*   Updated: 2023/05/13 03:36:57 by robhak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE	
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 4
 # endif
 
 # include <sys/types.h>
@@ -25,16 +25,29 @@
 # include <stdio.h>
 # include <stddef.h>
 
-typedef struct s_list
+typedef struct	s_file
 {
-	char				*content;
-	struct s_list		*next;
-}					t_list;
+	int				fd;
+	char			*content;
+	struct s_file	*next;
+}				t_file;
+
 int		gnl_strlen(char *s);
 char	*gnl_strcat(char *dest, char *src);
 char	*gnl_strchr(const char *s, int c);
 char	*gnl_strjoin(char *s1, char *s2);
 char	*gnl_strdup(const char *s);
 char	*gnl_strncpy(char *dest, const char *src, size_t n);
+
+typedef struct	s_list
+{
+	char			*content;
+	struct s_list	*next;
+}				t_list;
+
+char	*get_next_line(int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst);
 
 #endif
